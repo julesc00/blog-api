31c85c1b7205222938ad67a88f6de08c2818c99d
juliobriones <julesc00@hotmail.com>
2021-02-18 14:24:31 -0600

    Added user views.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
 from .models import Post
@@ -8,3 +10,10 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ("id", "author", "title", "body", "created_at")
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username",)
